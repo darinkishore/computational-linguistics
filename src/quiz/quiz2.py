1b77ad50b42bb405d090de6ed43bbab80b9d3e56
darinkishore <darinkishore@protonmail.com>
2022-12-04 16:20:10 +0530

    quiz 2!! lets go

--- src/quiz/quiz2.py
+++ src/quiz/quiz2.py
@@ -14,7 +14,9 @@
 # limitations under the License.
 # ========================================================================
 import json
+from collections import Counter
 from typing import Dict, Any, List
+import numpy as np
 
 from src.vector_space_models import tf_idfs, most_similar
 
@@ -61,23 +63,49 @@ FM = {
 
 
 def cosine(x1: Dict[str, float], x2: Dict[str, float]) -> float:
-    # TODO: to be updated
-    return 0
+    x1, x2 = generate_sparse_vector(x1, x2)
+    return np.inner(x1, x2) / (np.linalg.norm(x1) * np.linalg.norm(x2))
 
 
 def vectorize(documents: List[Dict[str, Any]]) -> Dict[str, Dict[str, int]]:
     # Feel free to update this function
     return tf_idfs(documents)
 
+def generate_sparse_vector(vec1, vec2): # stolen from stackexchange, makes it so the dimensions match
+    counter1 = Counter(vec1)
+    counter2 = Counter(vec2)
+    all_items = set(counter1.keys()).union(set(counter2.keys()))
+    vector1 = np.array([counter1[k] for k in all_items])
+    vector2 = np.array([counter2[k] for k in all_items])
+    return vector1, vector2
+
 
 def similar_documents(X: Dict[str, Dict[str, float]], Y: Dict[str, Dict[str, float]]) -> Dict[str, str]:
-    # Feel free to update this function
-    return {k: most_similar(Y, x) for k, x in X.items()}
+    # similar documents will now use cosine similarity instead of euclidean distance
+    # for each doc in x, find the most similar doc in y
+    # return a dictionary mapping the name of the document in X to the name of the document in Y
+    map = {}
+    for doc in X:
+        vec1 = X[doc]
+        for doc2 in Y:
+            vec2 = Y[doc2]
+            if doc not in map:
+                map[doc] = (doc2, cosine(vec1, vec2))
+            else:
+                if cosine(vec1, vec2) > map[doc][1]:
+                    map[doc] = (doc2, cosine(vec1, vec2))
+    # remove the scores
+    for doc in map:
+        map[doc] = map[doc][0]
+    return map
 
+# by changing it so similar documents used cosine similarity instead of euclidian distance
+# and creating sparse vector mappings for all of the documents such that cosine similarity would work
+# similar_documents is not more accurate. earlier, it could barely tell what the similar ones were.
 
 if __name__ == '__main__':
-    fables = json.load(open('res/vsm/aesopfables.json'))
-    fables_alt = json.load(open('res/vsm/aesopfables-alt.json'))
+    fables = json.load(open('../../res/vsm/aesopfables.json'))
+    fables_alt = json.load(open('../../res/vsm/aesopfables-alt.json'))
 
     v_fables = vectorize(fables)
     v_fables_alt = vectorize(fables_alt)
